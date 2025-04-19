083b0649c5ea6450dc55e7e58a0cf0b57eb6c368
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-19 21:54:22 +0600

    implement-search-functionality

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bannerImg from '../../assets/banner.png';
 import Button from '../ui/Button';
 
-const Banner = () => {
+const Banner = ({ handleSearch }) => {
+  const [searchText, setSearchText] = useState('');
+
   return (
     <div className="flex flex-col py-12 items-center">
       <img className="md:w-1/3" src={bannerImg} />
@@ -14,8 +16,17 @@ const Banner = () => {
         phones of the current time - FlagshipFaceOff
       </p>
 
-      <form className="flex flex-col md:flex-row justify-center gap-2 w-full items-center">
+      <form
+        onSubmit={e => {
+          handleSearch(e, searchText);
+          setSearchText('');
+          window.scrollTo(0, 400);
+        }}
+        className="flex flex-col md:flex-row justify-center gap-2 w-full items-center"
+      >
         <input
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
           className="bg-white px-4 h-12 border border-gray-300 rounded-md focus:outline-gray-300 w-full md:w-3/5 text-center"
           type="text"
           placeholder="Search Phone By Name"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../components/Banner/Banner';
 import PhoneContainer from '../components/PhoneContainer/PhoneContainer';
 import { useLoaderData } from 'react-router';
 
 const Home = () => {
-  const phones = useLoaderData();
+  const data = useLoaderData();
+  const [phones, setPhones] = useState(data);
+
+  const handleSearch = (e, text) => {
+    e.preventDefault();
+    if (text === '') return setPhones(data);
+
+    const searchedPhone = data.filter(
+      phone =>
+        phone?.name?.toLowerCase().split(' ').includes(text.toLowerCase()) ||
+        phone?.brand?.toLowerCase().split(' ').includes(text.toLowerCase())
+    );
+
+    setPhones(searchedPhone);
+  };
+
   return (
     <div>
-      <Banner />
+      <Banner handleSearch={handleSearch} />
       <PhoneContainer phones={phones} />
     </div>
   );
